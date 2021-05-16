d09aa5d0ede5e29b8e123feb4cee4a9f7055d864
Adolfovi <32370254+Adolfovi@users.noreply.github.com>
2021-05-16 20:45:55 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 var playeroption;
-
+var score=100;
 var cpuoption;
 
 // FUNCION PIEDRA
@@ -8,16 +8,20 @@ playeroption = 0;
 cpuoption = Math.floor(Math.random() * 3);
   if(playeroption === 0 && cpuoption === 0){
     document.getElementById('resultado').innerHTML="PIEDRA- PIEDRA: EMPATE!!";
-  
+  score=score+0;
+document.getElementById('points').innerHTML=score;
   }
 
   if(playeroption === 0 && cpuoption === 1){
     document.getElementById('resultado').innerHTML="PIEDRA- PAPEL: PERDISTES!!";
-  
+  score=score-1;
+  document.getElementById('points').innerHTML=score;
   }
 
   if(playeroption === 0 && cpuoption === 2){
     document.getElementById('resultado').innerHTML="PIEDRA- TIJERA: GANASTES!!";
+  score=score+2;
+document.getElementById('points').innerHTML=score;
   }
 
 
@@ -34,16 +38,20 @@ playeroption = 1;
 cpuoption = Math.floor(Math.random() * 3);
   if(playeroption === 1 && cpuoption === 0){
     document.getElementById('resultado').innerHTML="PAPEL- PIEDRA: GANASTES!!";
-  
+  score=score+2;
+  document.getElementById('points').innerHTML=score;
   }
 
   if(playeroption === 1 && cpuoption === 1){
     document.getElementById('resultado').innerHTML="PAPEL- PAPEL: EMPATE!!";
-  
+  score=score+0;
+  document.getElementById('points').innerHTML=score;
   }
 
   if(playeroption === 1 && cpuoption === 2){
     document.getElementById('resultado').innerHTML="PAPEL- TIJERA: PERDISTES!!";
+  score=score-1;
+document.getElementById('points').innerHTML=score;
   }
 
 
@@ -58,16 +66,20 @@ playeroption = 2;
 cpuoption = Math.floor(Math.random() * 3);
   if(playeroption === 2 && cpuoption === 0){
     document.getElementById('resultado').innerHTML="TIJERA- PIEDRA: PERDISTES!!";
-  
+  score=score-1;
+  document.getElementById('points').innerHTML=score;
   }
 
   if(playeroption === 2 && cpuoption === 1){
     document.getElementById('resultado').innerHTML="TIJERA- PAPEL: GANASTES!!";
-  
+  score=score+2;
+  document.getElementById('points').innerHTML=score;
   }
 
   if(playeroption === 2 && cpuoption === 2){
     document.getElementById('resultado').innerHTML="TIJERA- TIJERA: EMPATE!!";
+  score=score+0;
+document.getElementById('points').innerHTML=score;
   }
 
 
